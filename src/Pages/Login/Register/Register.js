967773d7a71e7af440c7f7d0aa1e68da4d0a7bc2
Reduwan-Nyx <arnodorain124@gmail.com>
2022-04-18 15:34:19 +0600

    email verification

--- src/Pages/Login/Register/Register.js
+++ src/Pages/Login/Register/Register.js
@@ -1,17 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import './Register.css'
 import auth from '../../../firebase.init'
-import { useAuthState, useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import { useAuthState, useCreateUserWithEmailAndPassword, useUpdateProfile } from 'react-firebase-hooks/auth';
 import SocialLogin from '../SocialLogin/SocialLogin';
 const Register = () => {
-
-    const [
-  createUserWithEmailAndPassword,
-  user,
-  loading,
-  error,
-] = useCreateUserWithEmailAndPassword(auth);
+const [agree, setAgree] = useState(false)
+    const [createUserWithEmailAndPassword, user, loading, error] =
+      useCreateUserWithEmailAndPassword(auth, { sendEmailVerification: true});
+      const [updateProfile, updating] = useUpdateProfile(auth);
 
 
     const navigate = useNavigate()
@@ -20,17 +17,23 @@ const Register = () => {
         navigate('/login')
     }
 
-    if(user){
-        navigate('/home')
-    }
+    
 
-    const handleRegister = event =>{
+    const handleRegister = async (event) =>{
         event.preventDefault()
         const name = event.target.name.value;
         const email = event.target.email.value;
         const password = event.target.password.value;
+        // const agree = event.target.terms.checked;
 
-        createUserWithEmailAndPassword(email, password)
+       
+        await createUserWithEmailAndPassword(email, password);
+          await updateProfile({ displayName: name });
+          console.log("Updated profile");
+           navigate("/home");
+        
+
+       
     }
 
     return (
@@ -42,9 +45,11 @@ const Register = () => {
                 <input type="email" name='email' id='' placeholder='Email Address' required/>
                 <br />
                 <input type="password" name='password' id='' placeholder='Password' required/>
-                <input type="checkbox" name="terms" id="terms" />
-                <label htmlFor="terms">Accept terms and Conditions</label>
-                <input className='w-50 mx-auto d-block btn btn-primary mt-3' type="submit" value="Register" />
+                <input onClick={() => setAgree(!agree)} type="checkbox" name="terms" id="terms" />
+                <label className={`ps-2 ${agree ? '' : 'text-danger'}`}  htmlFor="terms">Accept terms and Conditions</label>
+                <input
+                disabled={!agree}
+                className='w-50 mx-auto d-block btn btn-primary mt-3' type="submit" value="Register" />
             </form>
        <p>Already have a Account? <Link to="/login" className='text-primary pe-auto text-decoration-none' onClick={navigateLogin}>Login</Link> 
        </p>
